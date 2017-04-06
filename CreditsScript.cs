162e7f7c0c319ea5bfb5de4b55026c8e989dc164
CheTranqui <CheTranqui@users.noreply.github.com>
2017-04-06 00:07:16 -0400

    Alpha 1.0
    Text to accompany the zoomed in maps and the button script to manage the external links for credits/copyright recognition purposes.

---
+++ CreditsScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour {
+
+    private Button CreditsButton;
+    private Text person;
+    private string personString;
+    public string link;
+
+    void Start()
+    {
+        link = "none";
+        CreditsButton = this.gameObject.GetComponent<Button>();
+        CreditsButton.onClick.AddListener(delegate () { ShowCredits(); });
+    }
+
+    public void ShowCredits()
+    {
+        person = this.gameObject.GetComponentInChildren<Text>();
+        personString = person.text.ToString();
+
+        if (personString == "Blizzard")
+        {
+            link = "http://us.battle.net/heroes/en/";
+        }
+        if (personString == "Dreadnaught")
+        {
+            link = "https://www.youtube.com/watch?v=Hdzdksu20XM&t";
+        }
+        if (personString == "Cavalier Guest")
+        {
+            link = "https://www.youtube.com/watch?v=fHZ01eYLEPc";
+        }
+        if (personString == "Stolen")
+        {
+            link = "https://www.reddit.com/r/heroesofthestorm/comments/47m8qz/tips_for_every_map/";
+        }
+        if (personString == "Srey")
+        {
+            link = "https://www.youtube.com/watch?v=-ILfQUaTuv4";
+        }
+        if (personString == "Renewed Hope")
+        {
+            link = "https://renewedhope.guildlaunch.com";
+        }
+        if (personString == "CheTranqui")
+        {
+            link = "https://chetranqui.weebly.com";
+        }
+        Application.OpenURL(link);
+    }
+
+}
