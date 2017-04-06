a93172889dd739d60550514886d8225c0b804783
CheTranqui <CheTranqui@users.noreply.github.com>
2017-04-06 00:04:11 -0400

    Alpha 1.0
    Added support for credit buttons as well as zoomed in maps along with their associated text.

--- DropdownMapSwitcher.cs
+++ DropdownMapSwitcher.cs
@@ -13,7 +13,7 @@ public class DropdownMapSwitcher : MonoBehaviour
     public Dropdown mapDropdown;
     public Button mapButtonBoE;
     public Button mapButtonBBay;
-    public Button mapButtonBraxxis;
+    public Button mapButtonBraxis;
     public Button mapButtonCursedH;
     public Button mapButtonDShire;
     public Button mapButtonGarden;
@@ -25,7 +25,7 @@ public class DropdownMapSwitcher : MonoBehaviour
     public Button mapButtonWarheadJ;
     public GameObject mapObjectBoE;
     public GameObject mapObjectBBay;
-    public GameObject mapObjectBraxxis;
+    public GameObject mapObjectBraxis;
     public GameObject mapObjectCursedH;
     public GameObject mapObjectDShire;
     public GameObject mapObjectGarden;
@@ -37,27 +37,49 @@ public class DropdownMapSwitcher : MonoBehaviour
     public GameObject mapObjectWarheadJ;
     public GameObject HeroSelectionPanel;
     public GameObject PanelMapZoom;
-    public GameObject PanelMapText;
-    public GameObject PanelMapZoomImage;
+    public GameManager gm;
+    public Text PanelMapText1;
+    public Text PanelMapText2;
+    public Text PanelMapTextTitle;
+    public Image PanelMapZoomImage;
+    public GameObject PanelFullMiddle;
+    public Button StartButton;
+    public Button CreditsButton;
+    public Button CreditsBlizzard;
+    public Button CreditsDreadnaught;
+    public Button CreditsStolen;
+    public Button CreditsCavalier;
+    public Button CreditsSrey;
+    public Button CreditsDrew;
+    public Button CreditsMe;
     private int randomMap;
     public string map;
+    public string draftStage;
+
+    public string mapTextString;
+    IDictionary<string, IDictionary<string, string>> mapTextDict;
 
     public void Start()
     {
         map = "none";
         mapDropdown.value = 0;
+        gm = GameObject.Find("Main Camera").GetComponent<GameManager>();
+        mapTextString = File.ReadAllText(Application.dataPath + "/Resources/HotSMapStuff/MapText.json");
+        mapTextDict = JsonConvert.DeserializeObject<IDictionary<string, IDictionary<string, string>>>(mapTextString);
         mapDropdown.onValueChanged.AddListener(delegate { DropDownSelection(); });
+        CreditsButton.onClick.AddListener(delegate { ShowCreditsPanel(); });
     }
 
     public void DropDownSelection()
     {
+        PanelMapZoom.SetActive(false);
         switch (mapDropdown.value)
         {
             case 0:
                 map = "none";
                 mapObjectBoE.SetActive(false);
                 mapObjectBBay.SetActive(false);
-                mapObjectBraxxis.SetActive(false);
+                mapObjectBraxis.SetActive(false);
                 mapObjectCursedH.SetActive(false);
                 mapObjectDShire.SetActive(false);
                 mapObjectGarden.SetActive(false);
@@ -71,7 +93,7 @@ public class DropdownMapSwitcher : MonoBehaviour
             case 1:
                 mapObjectBoE.SetActive(false);
                 mapObjectBBay.SetActive(false);
-                mapObjectBraxxis.SetActive(false);
+                mapObjectBraxis.SetActive(false);
                 mapObjectCursedH.SetActive(false);
                 mapObjectDShire.SetActive(false);
                 mapObjectGarden.SetActive(false);
@@ -98,9 +120,9 @@ public class DropdownMapSwitcher : MonoBehaviour
                 break;
 
             case 4:
-                map = "Braxxis";
-                mapObjectBraxxis.SetActive(true);
-                mapButtonBraxxis.onClick.AddListener(delegate () { ZoomIn(); });
+                map = "Braxis";
+                mapObjectBraxis.SetActive(true);
+                mapButtonBraxis.onClick.AddListener(delegate () { ZoomIn(); });
                 break;
             case 5:
                 map = "CursedH";
@@ -150,10 +172,47 @@ public class DropdownMapSwitcher : MonoBehaviour
                 break;
         }
     }
+
     public void ZoomIn()
     {
-        //  Need to get zoomed in map images
-        //  Need to create map details text
-        //  use variable "map" to call to the appropriate zoom image.
+        Debug.Log(mapTextDict[map]["Name"]);
+        draftStage = gm.draftStage;
+        if (draftStage == "none")
+        {
+            mapDropdown.gameObject.SetActive(false);
+            StartButton.gameObject.SetActive(false);
+        }
+        PanelMapZoom.SetActive(true);
+        PanelMapZoomImage.sprite = Resources.Load<Sprite>("HotSMapStuff/ZoomMap" + map);
+        HeroSelectionPanel.SetActive(false);
+        PanelFullMiddle.SetActive(false);
+
+        string Text1 = mapTextDict[map]["Facts"];
+        string Text2 = mapTextDict[map]["Advice"];
+        string TextTitle = mapTextDict[map]["Name"];
+
+        PanelMapText1.text = Text1;
+        PanelMapText2.text = Text2;
+        PanelMapTextTitle.text = TextTitle;
+    }
+
+    public void ShowCreditsPanel()
+    {
+        PanelMapZoom.SetActive(true);
+        PanelMapTextTitle.text = "Source Links";
+        PanelMapText1.text = "";
+        PanelMapText2.text = "";
+        PanelMapZoomImage.sprite = Resources.Load<Sprite>("HotSPics/Credits");
+        PanelFullMiddle.SetActive(false);
+        HeroSelectionPanel.SetActive(false);
+        mapDropdown.gameObject.SetActive(false);
+        StartButton.gameObject.SetActive(false);
+        CreditsBlizzard.gameObject.SetActive(true);
+        CreditsDreadnaught.gameObject.SetActive(true);
+        CreditsStolen.gameObject.SetActive(true);
+        CreditsCavalier.gameObject.SetActive(true);
+        CreditsSrey.gameObject.SetActive(true);
+        CreditsDrew.gameObject.SetActive(true);
+        CreditsMe.gameObject.SetActive(true);
     }
 }
